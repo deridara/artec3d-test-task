886d009fc9c8da8ca0e421a1027f6602f4182100
dara <teriaderide@gmail.com>
2019-11-25 10:24:51 +0300

    reorganaize for github pages

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
     publicPath: "/"
   },
   module: {
@@ -43,5 +43,5 @@ module.exports = {
     ]
   },
 
-  devServer: { contentBase: "./dist", port: 3000, historyApiFallback: true }
+  devServer: { contentBase: "./docs", port: 3000, historyApiFallback: true }
 };
